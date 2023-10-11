0578dc09624248014434e8d78008ab0f81dceede
emagoediv <emagoedivplay@gmail.com>
2023-10-11 20:13:28 -0300

    aba de amigos online offiline

---
+++ pages/friends/js/loadFriends.js
@@ -0,0 +1,18 @@
+var friends = {
+    0: {
+        name: "Leornador Sai",
+        imgPath: "/1/profile.svg",
+        online: true
+    },
+    1: {
+        name: "Thiago",
+        imgPath: "/2/profile.svg",
+        online: true
+    },
+    2: {
+        name: "Murilo",
+        imgPath: "/3/profile.svg",
+        online: false
+    },
+}
+
